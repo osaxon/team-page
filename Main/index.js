f3342e0ba009019c1bd4bb8f239a6ee0660645a9
Oliver Saxon <oliverrsaxon@gmail.com>
2021-04-27 14:33:11 +0100

    FEAT: HTML file and directory created with inquierer responses

--- Main/index.js
+++ Main/index.js
@@ -1,37 +1,46 @@
 // need inquirer to prompt a series of questsions
-const genHTML = require('./utils/render.js');
-const inquirer = require('inquirer')
-const path = require('path');
-const fs = require('fs');
-const TeamMember = require('./utils/teamMembers.js');
+const genHTML = require("./utils/render.js");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+const TeamMember = require("./utils/teamMembers.js");
 
-const OUTPUT_DIR = path.resolve(__dirname, "output")
+const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
-//if(!fs.existsSync(OUTPUT_DIR)) {
-    //fs.mkdirSync(OUTPUT_DIR)
-//}
-//fs.writeFileSync(outputPath,genHTML,"utf-8");
 
 const teamMembers = [];
 const idArray = [];
 
-const manager = new TeamMember({name: 'oli', role: 'Manager'});
+const manager = new TeamMember({ name: "oli", role: "Manager" });
 
 teamMembers.push(manager);
 console.log(teamMembers);
 
-function init(){
+function init() {
+  function createManager() {
+    inquirer
+      .prompt({
+        type: "input",
+        message: "Enter the Managers name",
+        name: "name",
+      })
+      .then((res) => {
+        const mgr = new TeamMember({ name: res.name });
+        createFile(mgr);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
-    function createManager(){
-        inquirer.prompt({
-            type: 'input',
-            message: 'question1',
-            name: 'question1'
-        })
+  createManager();
+
+  function createFile(data) {
+    if (!fs.existsSync(OUTPUT_DIR)) {
+      fs.mkdirSync(OUTPUT_DIR);
     }
-
-    createManager();
+    fs.writeFileSync(outputPath, genHTML(data), "utf-8");
+  }
 }
 
 init();
